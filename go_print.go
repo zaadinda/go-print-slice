64c3d06dd5cb982e376b2451146b27a14a7ec844
zaadinda <dindaayaskur30@gmail.com>
2025-02-20 20:24:37 +0700

    fix func print2D to add spacing between numbers

--- go_print.go
+++ go_print.go
@@ -11,7 +11,7 @@ func Print(slice []int) {
 func Print2D(slice [][]int) {
 	for i := 0; i < len(slice); i++ {
 		for j := 0; j < len(slice[i]); j++ {
-			fmt.Print(slice[i][j])
+			fmt.Print(slice[i][j], "\t")
 		}
 		fmt.Println()
 	}
